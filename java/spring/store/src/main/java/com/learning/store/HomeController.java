436743f86426763fb915468086170ee0def231bb
rahulsaamanth <rahulsaamanth@yahoo.com>
2025-07-24 12:09:29 +0530

    add: learning .env variables in spring boot

--- java/spring/store/src/main/java/com/learning/store/HomeController.java
+++ java/spring/store/src/main/java/com/learning/store/HomeController.java
@@ -1,12 +1,22 @@
 package com.learning.store;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @Value("${app.page-size}")
+    private int pageSize;
+
     @RequestMapping("/")
     public String index() {
+        System.out.println("Application Name: " + appName);
+        System.out.println("page size: " + pageSize);
         return "index.html";
     }
+
 }
