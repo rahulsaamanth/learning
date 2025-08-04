be8a7ab6f6d71dd134f48d8518909ffa251087ae
rahulsaamanth <rahulsaamanth@yahoo.com>
2025-08-04 20:31:14 +0530

    add: learnt java collections

--- java/learning/Main.java
+++ java/learning/Main.java
@@ -1,14 +1,26 @@
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
-    public static void main(String[] args){
-        Random rand1 = new Random(42);
-        Random rand2 = new Random(42);
+    public static void main(String[] args) {
 
-        double result = Math.floor(3.99);
+        List<Integer> list = new ArrayList<>();
+        list.addAll(List.of(1, 2, 3, 4, 5));
 
-//        System.out.println(rand1.nextInt());
-//        System.out.println(rand2.nextInt());
-        System.out.println(result);
+        list.removeIf(n -> n % 2 == 0);
+        System.out.println(list);
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        map.put("rahulsaamanth", null);
+        map.put("Jithendra", 10);
+
+        System.out.println(map);
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
     }
 }
