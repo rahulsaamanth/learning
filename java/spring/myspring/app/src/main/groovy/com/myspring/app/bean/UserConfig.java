f4490890db886e7f4afd9805b7b8d5b5bf064219
rahulsaamanth <rahulsaamanth@yahoo.com>
2025-09-02 00:02:20 +0530

    add: leart initializing and using Beans without annotations, with constructor and factor-method methods

---
+++ java/spring/myspring/app/src/main/groovy/com/myspring/app/bean/UserConfig.java
@@ -0,0 +1,15 @@
+package com.myspring.app.bean;
+
+public class UserConfig {
+
+    private String name;
+    private String className;
+
+    public void setName(final String name){
+        this.name = name;
+    }
+
+    public void setClassName(final String className){
+        this.className = className;
+    }
+}
---
+++ java/spring/myspring/app/src/main/groovy/com/myspring/app/bean/service/UserService.java
@@ -0,0 +1,11 @@
+package com.myspring.app.bean.service;
+
+import com.myspring.app.bean.UserConfig;
+
+public class UserService {
+    UserConfig userConfig;
+
+    public UserService(UserConfig userConfig){
+        this.userConfig = userConfig;
+    }
+}
